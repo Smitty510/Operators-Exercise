23d16063a3b57f951374650acc558cd890082467
patrick smith <smithpatrick.510@gmail.com>
2023-06-20 16:06:05 -0400

    operators

---
+++ OperatorExercise/Bonus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Bonus
+    {
+
+
+        public static void AreaOfCircle(double radius)
+        {
+            double area;
+
+            area = (Math.PI * radius * radius);
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+        }
+
+
+
+
+    }
+}
--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Excercise1
+
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+
+
+
+
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+
+            //Exercise 2: see Bonus Tab
+            {
+                Console.WriteLine("What is the radius of your circle?");
+                var radius = double.Parse(Console.ReadLine());
+                Bonus.AreaOfCircle(radius);
+            }
+
+
+
+
         }
-    }
+
+    }        
 }
+
+
+
+
+
